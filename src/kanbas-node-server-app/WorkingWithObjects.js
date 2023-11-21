2f600f8ee373ac689a72a27409db38623be8a529
dhritia <81909460+dhritia@users.noreply.github.com>
2023-11-20 21:59:13 -0500

    Update WorkingWithObjects.js

--- src/kanbas-node-server-app/WorkingWithObjects.js
+++ src/kanbas-node-server-app/WorkingWithObjects.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+const API_BASE = process.env.LAB_BASE;
 function WorkingWithObjects() {
   const [assignment, setAssignment] = useState({
     id: 1,
@@ -9,7 +10,7 @@ function WorkingWithObjects() {
     completed: false,
     score: 0,
   });
-  const URL = "http://localhost:4000/a5/assignment";
+  const URL = "{API_BASE}/a5/assignment";
   const fetchAssignment = async () => {
     const response = await axios.get(`${URL}`);
     setAssignment(response.data);
@@ -26,13 +27,13 @@ function WorkingWithObjects() {
     <div>
       <h3>Working With Objects</h3>
       <h4>Retrieving Objects</h4>
-      <a href="http://localhost:4000/a5/assignment"
+      <a href={`${API_BASE}/a5/assignment`}
          className="btn btn-primary me-2">
         Get Assignment
       </a>
       <h4>Retrieving Properties</h4>
       <a
-        href="http://localhost:4000/a5/assignment/title"
+        href={`${API_BASE}/a5/assignment/title`}
         className="btn btn-primary me-2">
         Get Title
       </a>
