7b4dd123ac7908990f53f9806580e8aa3f0822a3
dhritia <dhritianjaria@gmail.com>
2023-12-03 11:16:12 -0500

    Update app.js

--- src/kanbas-node-server-app/app.js
+++ src/kanbas-node-server-app/app.js
@@ -8,7 +8,7 @@ import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 import UserRoutes from "./users/routes.js";
 const app = express();
